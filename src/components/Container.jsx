378067131b8d941d217e7132043f396c9fa92c76
TranscriptionToolPPT <mohamed95hassan12@gmail.com>
2026-01-15 13:41:08 +0400

    Update Container.jsx

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,1 +1,8 @@
+export default function Container({ className = "", children }) {
+  return (
+    <div className={`mx-auto w-full max-w-screen-2xl px-6 md:px-10 ${className}`}>
+      {children}
+    </div>
+  )
+}
 
